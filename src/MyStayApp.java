6fbc09aadd033d21871c63cecaa11abd469a3578
AdityaKdm <adi91.kdm@gmail.com>
2026-03-12 08:44:09 +0530

    Base MyStayApp setup

---
+++ src/MyStayApp.java
@@ -0,0 +1,10 @@
+public class MyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("         WELCOME TO MYSTAY       ");
+        System.out.println("=================================");
+        System.out.println("MyStay App Started Successfully!");
+    }
+}
